500a6171a06fb79212d9ac49b2c314ece76fcd31
yareyaska <uraharahussein@gmail.com>
2019-05-30 03:53:18 -0400

    add files

---
+++ js/script.js
@@ -0,0 +1,33 @@
+var a = parseInt(prompt("enter num1"));
+var b = parseInt(prompt("enter num2"));
+var add = function(a,b){
+return a+b
+
+};
+alert("the sum of " + a + " and " + b + " is " + add(a,b));
+
+var subtraction = function(a,b){
+return a-b
+
+};
+alert("the difference btwn  " + a + " and " + b + " is " + subtraction(a,b));
+
+
+
+var divide = function(a,b){
+return a/b
+
+};
+alert("the division of " + a + " and " + b + " is " + divide(a,b));
+
+ var multiply = function(a,b){
+return a*b
+
+};
+alert("the product of " + a + " and " + b + " is " + multiply(a,b));
+
+var remainder = function(a,b){
+return a%b
+
+};
+alert("the remainder of " + a + " and " + b + " is " + remainder(a,b));
